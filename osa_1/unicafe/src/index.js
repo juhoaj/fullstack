4cd0b01b2b78116fc1c5c7a5af31a90ef4a8deb2
Juho Juutilainen <juho.juutilainen@meteoriitti.com>
2019-01-27 22:38:15 +0200

    1.6 tehty

--- osa_1/unicafe/src/index.js
+++ osa_1/unicafe/src/index.js
@@ -1,6 +1,19 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = (props) => {
+
+}
+
+const Statistic = (props) => {
+
+}
+
+const Button = (props) => {
+    
+}
+
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -9,7 +22,14 @@ const App = () => {
 
   return (
     <div>
-      code here
+        <h3>Anna palautetta</h3>
+        <button onClick={() => setGood(good + 1)}>kasvata</button>
+        <button onClick={() => setNeutral(neutral + 1)}>kasvata</button>
+        <button onClick={() => setBad(bad + 1)}>kasvata</button>
+        <h3>Statistiikka</h3>
+        <p>hyvä: {good}</p>
+        <p>neutraali: {neutral}</p>
+        <p>huono: {bad}</p>
     </div>
   )
 }
