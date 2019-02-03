b87bed982177f4f6a1f9a3c980f618b52d9a1bc2
Juho Juutilainen <juho.juutilainen@meteoriitti.com>
2019-02-03 11:49:24 +0200

    2.5 asti tehty

--- osa_2/kurssitiedot/src/index.js
+++ osa_2/kurssitiedot/src/index.js
@@ -2,65 +2,88 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-const App = () => {
-    const course = {
-        name: 'Half Stack -sovelluskehitys',
-        parts: [
-            {
-                name: 'Reactin perusteet',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Tiedonvälitys propseilla',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'Komponenttien tila',
-                exercises: 14,
-                id: 3
-            }
-        ]
-    }
 
+const App = () => {
+    const courses = [
+        {
+            name: 'Half Stack -sovelluskehitys',
+            id: 1,
+            parts: [
+                {
+                    name: 'Reactin perusteet',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Tiedonvälitys propseilla',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'Komponenttien tila',
+                    exercises: 14,
+                    id: 3
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 2,
+                    id: 1
+                },
+                {
+                    name: 'Middlewaret',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
+    console.log(courses)
     return (
         <div>
-            <Course course={course} />
+            <Courses courses={courses}/>
         </div>
     )
 }
 
-const Course = ({course}) => (
+const Courses = ({ courses }) => courses.map(course =>
+    <div>
+        <Course course = {course}/>
+    </div>
+)
+
+const Course = ({ course }) => (
     <div>
         {console.log('name', course.name)}
-        <Header title = {course.name}/>
-        <Parts parts ={course.parts}/>
-        <Total parts = {course.parts}/>
+        <Header title={course.name} />
+        <Parts parts={course.parts} />
+        <Total parts={course.parts} />
     </div>
 )
 
 
-const Header = ({title}) => (
+const Header = ({ title }) => (
     <h1>{title}</h1>
 )
 
-const Parts = ({parts}) => parts.map(part =>
+const Parts = ({ parts }) => parts.map(part =>
     <p key={part.id}>
         {part.name} {part.exercises}
     </p>
 )
 
 
-const Total = ({parts}) => {
+const Total = ({ parts }) => {
     const summa = parts.reduce((a, b) => a + b.exercises, 0);
     console.log(summa)
     return (
         <p>yhteensä {summa} tehtävää</p>
     )
 }
-
-
-
 
 ReactDOM.render(<App />, document.getElementById('root'))
