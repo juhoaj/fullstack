1607ab2d6e817b8092f63abb42347af24c7cbd68
Juho Juutilainen <juho.juutilainen@meteoriitti.com>
2019-02-16 22:43:12 +0200

    4.4 valmis

--- osa_4/blogilista/utils/list_helper.js
+++ osa_4/blogilista/utils/list_helper.js
@@ -1,7 +1,12 @@
 const dummy = (blogs) => {
     return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+}
+
+const totalLikes = function(blogs) {
+    return blogs.reduce((summa, bloggaus) => summa + bloggaus.likes, 0) || 0
+} 
+
+module.exports = {
+    dummy, 
+    totalLikes
+}
