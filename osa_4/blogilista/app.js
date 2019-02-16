55f009b9370bd854d3d9137ce872e8382213c37f
Juho Juutilainen <juho.juutilainen@meteoriitti.com>
2019-02-16 21:59:17 +0200

    4.2 valmis

--- osa_4/blogilista/app.js
+++ osa_4/blogilista/app.js
@@ -2,50 +2,23 @@ const config = require('./utils/config')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const blogsRouter = require('./controllers/blogs')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+console.log('connecting to', config.MONGODB_URI)
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
-
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connection to MongoDB:', error.message)
+    })
 
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
