e1a17f1463ef85758b9dfd1ffd6b5b5a081b93b8
Juho Juutilainen <juho.juutilainen@meteoriitti.com>
2019-02-18 21:04:14 +0200

    4.19 tehty

--- osa_4/blogilista/controllers/blogs.js
+++ osa_4/blogilista/controllers/blogs.js
@@ -1,29 +1,43 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs.map(blog => blog.toJSON()))
 })
 
-
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
-    // const user = await User.findById(body.userId)
-    user = await User.findOne()
-    /* const blog = new Blog(request.body)*/
 
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes,
-        user: user._id
-    })
+    const token = getTokenFrom(request)
 
     try {
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+        if (!token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' })
+        }
+
+        const user = await User.findById(decodedToken.id)
+
+        const blog = new Blog({
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes,
+            user: user._id
+        })
+
         const savedBlog = await blog.save()
 
         user.blogs = user.blogs.concat(savedBlog._id)
@@ -32,7 +46,6 @@ blogsRouter.post('/', async (request, response, next) => {
     } catch (exception) {
         next(exception)
     }
-
 })
 
 blogsRouter.delete('/:id', (request, response, next) => {
